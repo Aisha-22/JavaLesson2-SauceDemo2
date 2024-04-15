edec7779b4cf548eb8275e564ffc94c29f5cbe83
Aishia <51677946+Aisha-22@users.noreply.github.com>
2024-04-15 17:30:20 +0200

    Updates on Firefox

--- src/test/java/FirefoxBrowser.java
+++ src/test/java/FirefoxBrowser.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirefoxBrowser {
@@ -14,6 +16,22 @@ public class FirefoxBrowser {
 //        The child carries the Browser functionalities
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
+
+        //        Maximize the Window
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+
+//        We have a 'Product' Element - Go to the Element and get the text of that Element
+        String product = driver.findElement(By.xpath("//span[contains(.,'Products')]")).getText();
+
+        //Assertions to Verify
+        Assert.assertEquals(product, "Expected");
+
+        driver.close();
 
     }
 }
