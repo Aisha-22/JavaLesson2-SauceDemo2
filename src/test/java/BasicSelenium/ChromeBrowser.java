9d01db8ed1aeedc7d82fad29322b23c93bcdef88
Aishia <51677946+Aisha-22@users.noreply.github.com>
2024-04-17 15:07:58 +0200

    Updated folder for Chrom and Firefox Browser

--- src/test/java/BasicSelenium/ChromeBrowser.java
+++ src/test/java/BasicSelenium/ChromeBrowser.java
@@ -1,23 +1,26 @@
+package BasicSelenium;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class FirefoxBrowser {
-
+public class ChromeBrowser {
+    //    declare a WebDriver (Creating an instance of a WebDriver)
     WebDriver driver;
 
+    //    To make the below test Executable we use testNG
     @Test
 //    Methode - Methode name should start with LowerCase those are the coding standards
-    public void startFirefoxBrowser() {
+    public void startChromeBrowser() {
 
 //        The child carries the Browser functionalities
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
-
-        //        Maximize the Window
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+//        Maximize the Window
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -31,7 +34,11 @@ public class FirefoxBrowser {
         //Assertions to Verify
         Assert.assertEquals(product, "Expected");
 
+
         driver.close();
 
+
     }
+
+
 }
--- src/test/java/BasicSelenium/FirefoxBrowser.java
+++ src/test/java/BasicSelenium/FirefoxBrowser.java
@@ -1,24 +1,25 @@
+package BasicSelenium;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class ChromeBrowser {
-    //    declare a WebDriver (Creating an instance of a WebDriver)
+public class FirefoxBrowser {
+
     WebDriver driver;
 
-    //    To make the below test Executable we use testNG
     @Test
 //    Methode - Methode name should start with LowerCase those are the coding standards
-    public void startChromeBrowser() {
+    public void startFirefoxBrowser() {
 
 //        The child carries the Browser functionalities
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-//        Maximize the Window
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+
+        //        Maximize the Window
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -32,11 +33,7 @@ public class ChromeBrowser {
         //Assertions to Verify
         Assert.assertEquals(product, "Expected");
 
-
         driver.close();
 
-
     }
-
-
 }
